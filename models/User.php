7e25d6ef6245842f67a3c5aeb626ef8a17dfbee9
zhura <zhuravlevkos9@gmail.com>
2025-03-30 22:22:30 +0700

    Create authorization

---
+++ models/User.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "user".
+ *
+ * @property int $id
+ * @property string $username
+ * @property string $password
+ * @property string $FIO
+ * @property string $number
+ * @property string $email
+ * @property string $role
+ */
+class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'user';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['username', 'password', 'FIO', 'number', 'email'], 'required'],
+            [['username', 'password', 'FIO', 'number', 'email'], 'string', 'max' => 50],
+            [['role'], 'string', 'max' => 30],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'password' => 'Password',
+            'FIO' => 'Fio',
+            'number' => 'Number',
+            'email' => 'Email',
+            'role' => 'Role',
+        ];
+    }
+    public static function findIdentity($id)
+    {
+        return static::findOne($id);;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+
+    public static function findIdentityByAccessToken($token, $type = null)
+    {
+        return null;
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByUsername($username)
+    {
+        return  User::findOne(['username' => $username]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthKey()
+    {
+        return null;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validateAuthKey($authKey)
+    {
+        return false;
+    }
+
+    /**
+     * Validates password
+     *
+     * @param string $password password to validate
+     * @return bool if password provided is valid for current user
+     */
+    public function validatePassword($password)
+    {
+        return $this->password === $password;
+    }
+}
